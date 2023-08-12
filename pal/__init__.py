9fe67606f6df743b9a43dd8a530f01795e793e1f
yunnerwm <yxm9326@mavs.uta.edu>
2023-08-12 17:16:00 -0500

    add necessary file __init__.py for a python module

---
+++ pal/__init__.py
@@ -0,0 +1,6 @@
+from .core import interface, runtime
+
+# in package of the python modules, must contain the __init__.py file, otherview this will only resulting in a directory rather than a package. 
+# Then cannot be import or contain other module.
+# Usually this file is empty
+#  
